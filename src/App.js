a410ea6ad855852034198d9e93f3de142c5c14b8
Jeff Juszczak <jeff@moottoast.com>
2016-11-12 14:03:43 -0500

    Firebase are belong to us

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: 'AIzaSyARPbg_BOaoS1j-9KdKCXZQ8YiZ7vnilMc',
+    authDomain: 'auth-tutorial-3b05b.firebaseapp.com',
+    databaseURL: 'https://auth-tutorial-3b05b.firebaseio.com',
+    storageBucket: 'auth-tutorial-3b05b.appspot.com',
+    messagingSenderId: '746375122459'
+  });
+  }
   render() {
     return (
       <View>
+        <Header headerText='Authentication' />
         <Text>Auth App</Text>
       </View>
     );
