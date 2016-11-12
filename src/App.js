026cc70be69c31e07caed729e1734f110e6b9890
Jeff Juszczak <jeff@moottoast.com>
2016-11-12 15:24:26 -0500

    User email input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   componentWillMount() {
     firebase.initializeApp({
-    apiKey: 'AIzaSyARPbg_BOaoS1j-9KdKCXZQ8YiZ7vnilMc',
-    authDomain: 'auth-tutorial-3b05b.firebaseapp.com',
-    databaseURL: 'https://auth-tutorial-3b05b.firebaseio.com',
-    storageBucket: 'auth-tutorial-3b05b.appspot.com',
-    messagingSenderId: '746375122459'
-  });
+      apiKey: 'AIzaSyARPbg_BOaoS1j-9KdKCXZQ8YiZ7vnilMc',
+      authDomain: 'auth-tutorial-3b05b.firebaseapp.com',
+      databaseURL: 'https://auth-tutorial-3b05b.firebaseio.com',
+      storageBucket: 'auth-tutorial-3b05b.appspot.com',
+      messagingSenderId: '746375122459'
+    });
   }
   render() {
     return (
       <View>
-        <Header headerText='Authentication' />
-        <Text>Auth App</Text>
+        <Header headerText="Authentication" />
+        <LoginForm />
       </View>
     );
   }
